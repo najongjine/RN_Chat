f0ff5d1042146e263c695d1a29454d657eda3f7b
najongjine <najongjin3@hotmail.com>
2026-05-18 09:37:04 +0900

    .

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+// src/app/_layout.tsx
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { Stack } from "expo-router";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "홈",
+        }}
+      />
+
+      <Stack.Screen
+        name="explore"
+        options={{
+          title: "탐색",
+        }}
+      />
+    </Stack>
   );
 }
